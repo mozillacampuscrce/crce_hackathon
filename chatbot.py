c1696f8d6d88db83c3b3b17990934d35f365970e
Mohil Palav <mohilpalav@gmail.com>
2019-03-31 00:09:21 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,9 @@ from nltk.corpus import stopwords
 import string
 import random
 from nltk.stem import PorterStemmer
-
+from allennlp.predictors.predictor import Predictor
+from chatbotscraper import newsapi
 def preprocess(input):
-
     token = nltk.word_tokenize(input)
     remove_tokens = set(stopwords.words('english')+list(string.punctuation))
     ps = PorterStemmer()
@@ -15,18 +15,14 @@ def preprocess(input):
             filtered_tokens.append(ps.stem(w))
     return filtered_tokens
 
-def reply(input):
+def reply(input,user):
     greeting_inputs = ["hello", "hi", "greetings", "sup", "what's up","hey"]
     greeting_response = ["hi!", "hey!", "hi there!", "hello!"]
-
     thanking_inputs = ["thank","thankyou"]
     thanking_response = ["You are welcome.","Always a pleasure."]
-
     leaving_inputs = ["bye"]
     leaving_response = ["Bye! Take care!","GoodBye! See you later!"]
-
     no_response = ["Sorry! I can't answer this question.", "Sorry! Maybe try asking something else?"]
-
     for word in input:
         if word in greeting_inputs:
             return random.choice(greeting_response)
@@ -35,8 +31,24 @@ def reply(input):
         if word in thanking_inputs:
             return random.choice(thanking_response)
         else:
-            #### AllenNLP ####
-            return random.choice(no_response)
+            try:
+                query = ""
+                phrase = ""
+                answers = []
+                for i in input:
+                    query=query + " " + i
+                apiresponseinit=newsapi(query)
+                apiresponse=apiresponseinit.getcontent()
+                for i in range(3):
+                    predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz")
+                    x1 = predictor.predict(
+                        passage=apiresponse[i].text,
+                        question=user
+                    )
+                    answers.append(x1['best_span_str'])
+                return answers
+            except:
+                return random.choice(no_response)
 
 user = ""
 flag = True
